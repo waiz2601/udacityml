8cce6f0a3f6f6b259bad356fc142c45da3239228
Waiz2601 <syedwaiz2601@gmail.com>
2025-03-09 02:40:29 +0530

    svm done

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
-""" 
-    This is the code to accompany the Lesson 1 (Naive Bayes) mini-project. 
 
-    Use a Naive Bayes Classifier to identify emails by their authors
-    
-    authors and labels:
-    Sara has label 0
-    Chris has label 1
-"""
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from time import time
@@ -20,46 +12,24 @@ sys.path.append("../tools/")
 from email_preprocess import preprocess
 
 
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
-##############################################################
-# Enter Your Code Here
 
 clf = GaussianNB()
 
-# Train the classifier and measure the training time
 t0 = time()
 clf.fit(features_train, labels_train)
 print("Training Time:", round(time()-t0, 3), "s")
 
-# Predict the labels for the test data and measure the prediction time
+
 t0 = time()
 pred = clf.predict(features_test)
 print("Predicting Time:", round(time()-t0, 3), "s")
 
-# Calculate and print the accuracy
 accuracy = accuracy_score(labels_test, pred)
 print("Accuracy:", accuracy)
 
-##############################################################
 
-##############################################################
-'''
-You Will be Required to record time for Training and Predicting 
-The Code Given on Udacity Website is in Python-2
-The Following Code is Python-3 version of the same code
-'''
 
-# t0 = time()
-# # < your clf.fit() line of code >
-# print("Training Time:", round(time()-t0, 3), "s")
-
-# t0 = time()
-# # < your clf.predict() line of code >
-# print("Predicting Time:", round(time()-t0, 3), "s")
-
-##############################################################
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,12 +12,33 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC # Support Vector Classifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
+
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
+
+
+clf=SVC(kernel='rbf')
+t0 = time()
+clf.fit(features_train, labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+t1=time()
+pred=clf.predict(features_test)
+print("Prediction Time:", round(time()-t1, 3), "s")
+accuracy = accuracy_score(labels_test, pred)
+print("Accuracy:", accuracy)
+
 
 
 #########################################################
@@ -33,7 +54,5 @@ But the Code provided in Quiz has an Indexing issue
 The Code Below solves that issue, So use this one
 '''
 
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
 
 #########################################################
