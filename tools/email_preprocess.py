7e9c0ca983925d8eb27c66c463489c499ab7481b
Waiz2601 <syedwaiz2601@gmail.com>
2025-03-08 16:33:07 +0530

    first commit

--- tools/email_preprocess.py
+++ tools/email_preprocess.py
@@ -7,54 +7,48 @@ from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.feature_selection import SelectPercentile, f_classif
 
-
-def preprocess(words_file = "../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
+def preprocess(words_file="../tools/word_data.pkl", authors_file="../tools/email_authors.pkl"):
     """ 
-        this function takes a pre-made list of email texts (by default word_data.pkl)
+        This function takes a pre-made list of email texts (by default word_data.pkl)
         and the corresponding authors (by default email_authors.pkl) and performs
         a number of preprocessing steps:
             -- splits into training/testing sets (10% testing)
             -- vectorizes into tfidf matrix
             -- selects/keeps most helpful features
 
-        after this, the feaures and labels are put into numpy arrays, which play nice with sklearn functions
+        After this, the features and labels are put into numpy arrays, which play nice with sklearn functions
 
         4 objects are returned:
             -- training/testing features
             -- training/testing labels
-
     """
 
-    ### the words (features) and authors (labels), already largely preprocessed
-    ### this preprocessing will be repeated in the text learning mini-project
-    authors_file_handler = open(authors_file, "rb")
-    authors = joblib.load(authors_file_handler)
+    try:
+        # Load the authors and word data
+        with open(authors_file, "rb") as authors_file_handler:
+            authors = joblib.load(authors_file_handler, encoding="latin1")
+        with open(words_file, "rb") as words_file_handler:
+            word_data = joblib.load(words_file_handler, encoding="latin1")
+    except Exception as e:
+        print(f"Error loading data: {e}")
+        return None, None, None, None
 
-
-    words_file_handler = open(words_file, "rb")
-    word_data = joblib.load(words_file_handler)
-
-    ### test_size is the percentage of events assigned to the test set
-    ### (remainder go into training)
+    # Split into training and testing sets
     features_train, features_test, labels_train, labels_test = train_test_split(word_data, authors, test_size=0.1, random_state=42)
 
-
-    ### text vectorization--go from strings to lists of numbers
+    # Text vectorization--go from strings to lists of numbers
     vectorizer = TfidfVectorizer(sublinear_tf=True, max_df=0.5, stop_words='english')
     features_train_transformed = vectorizer.fit_transform(features_train)
-    features_test_transformed  = vectorizer.transform(features_test)
+    features_test_transformed = vectorizer.transform(features_test)
 
-
-
-    ### feature selection, because text is super high dimensional and 
-    ### can be really computationally chewy as a result
+    # Feature selection, because text is super high dimensional and can be really computationally chewy as a result
     selector = SelectPercentile(f_classif, percentile=10)
     selector.fit(features_train_transformed, labels_train)
     features_train_transformed = selector.transform(features_train_transformed).toarray()
-    features_test_transformed  = selector.transform(features_test_transformed).toarray()
+    features_test_transformed = selector.transform(features_test_transformed).toarray()
 
-    ### info on the data
-    print("No. of Chris training emails : ", sum(labels_train))
-    print("No. of Sara training emails : ", len(labels_train)-sum(labels_train))
-    
+    # Info on the data
+    print("No. of Chris training emails:", sum(labels_train))
+    print("No. of Sara training emails:", len(labels_train) - sum(labels_train))
+
     return features_train_transformed, features_test_transformed, labels_train, labels_test
