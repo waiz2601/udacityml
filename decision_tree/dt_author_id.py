84fd53b069b252dbbc3977b5384bf2e7137ee4c1
Waiz2601 <syedwaiz2601@gmail.com>
2025-03-09 13:16:14 +0530

    DecisionTreeDone

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,12 +12,25 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+
+clf=DecisionTreeClassifier(min_samples_split=40)
+t0=time()
+clf.fit(features_train,labels_train)
+print("Training Time:", round(time()-t0, 3), "s")
+pred=clf.predict(features_test)
+accuracy_score=accuracy_score(labels_test,pred)
+print("Accuracy:", accuracy_score)
+
+
+
 
 
 
